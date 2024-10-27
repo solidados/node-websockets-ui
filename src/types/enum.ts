ceddcfee0892600a74a70ccb5feae02c3b3481d0
solidados <geekbrains.pk@internet.ru>
2024-10-27 16:27:13 +0400

    feat: add enums with message types

---
+++ src/types/enum.ts
@@ -0,0 +1,15 @@
+export enum MESSAGE_TYPES {
+  REGISTRATION = 'reg',
+  UPDATE_WINNERS = 'update_winners',
+  CREATE_ROOM = 'create_room',
+  CREATE_GAME = 'create_game',
+  ADD_USER_TO_ROOM = 'add_user_to_room',
+  UPDATE_ROOM = 'update_room',
+  ADD_SHIPS = 'add_ships',
+  START_GAME = 'start_game',
+  RANDOM_ATTACK = 'randomAttack',
+  TURN = 'turn',
+  ATTACK = 'attack',
+  FINISH = 'finish',
+  SINGLE_PLAY = 'single_play',
+}
