d1f54faf894607858cbdd8d30f137b8d06c0b7f9
solidados <geekbrains.pk@internet.ru>
2024-10-28 15:41:32 +0400

    refactor: update addShips to controllers

--- src/controllers/addShips.ts
+++ src/controllers/addShips.ts
@@ -4,16 +4,22 @@ import { changeTurn } from './';
 import { botShips } from '../utils/botStrategy';
 import { getRandomNumber } from '../utils/getRandomNumber';
 import { startGameResponse } from '../utils/handleResponseMessages';
-import { IShip, WebSocketClient } from '../types/interfaces';
+import {
+  IGame,
+  IGamePlayer,
+  IShip,
+  WebSocketClient,
+} from '../types/interfaces';
 
 const addShips = (gameId: number, ships: IShip[], ws: WebSocketClient) => {
   const { findGame, sockets } = db;
-  const game = findGame(gameId);
+  const game: IGame = findGame(gameId);
+
   if (game) {
     game.ships[ws.index] = new Game(ships);
     if (Object.keys(game.ships).length === 2 && !game.withBot) {
-      game.players.forEach((player) => {
-        const message = startGameResponse(
+      game.players.forEach((player: IGamePlayer) => {
+        const message: string = startGameResponse(
           game.ships[player.index].ships,
           player.index,
         );
@@ -24,17 +30,17 @@ const addShips = (gameId: number, ships: IShip[], ws: WebSocketClient) => {
     }
 
     if (game.withBot) {
-      const randomShipsIndex = getRandomNumber(0, botShips.length - 1);
+      const randomShipsIndex: number = getRandomNumber(0, botShips.length - 1);
       const randomShips = botShips[randomShipsIndex];
       game.ships[-1] = new Game(randomShips);
-      const message = startGameResponse(ships, ws.index);
+
+      const message: string = startGameResponse(ships, ws.index);
       sockets[ws.index]!.send(message);
-      console.log('Message sent:', message);
+      console.log(`\x1b[32mMessage sent: \x1b[92m${message}\x1b[0m`);
       changeTurn(gameId);
     }
-    // console.log('game', game);
   } else {
-    console.log('Game not found');
+    console.log(`\x1b[93mGame was not found. Try again...\x1b[0m`);
   }
 };
 
