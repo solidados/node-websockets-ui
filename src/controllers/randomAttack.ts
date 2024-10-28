d33a3fd29a6fdbcaccd60b36da41d6fdd02b31c2
solidados <geekbrains.pk@internet.ru>
2024-10-28 15:47:04 +0400

    feat: add randomAttack.ts logic to controllers

---
+++ src/controllers/randomAttack.ts
@@ -0,0 +1,44 @@
+import { db } from '../db';
+import { Board } from '../db/board';
+import { attack } from './';
+import { IGame, ITile, WebSocketClient } from '../types/interfaces';
+import { getRandomNumber } from '../utils/getRandomNumber';
+
+const randomAttack = (data: string, ws: WebSocketClient) => {
+  // console.log('randomAttack', data);
+  const { gameId, indexPlayer } = JSON.parse(data);
+  const { findGame, findEnemy } = db;
+  const game: IGame = findGame(gameId);
+
+  if (!game) return;
+
+  const eIndex: number = findEnemy(game, indexPlayer);
+  const eBoard: Board[][] = game.ships[eIndex].gameBoard;
+
+  const tiles: ITile[] = [];
+
+  eBoard.forEach((row, x) => {
+    row.forEach((tile, y) => {
+      if (!tile.checked) {
+        tiles.push({ x, y });
+      }
+    });
+  });
+
+  if (tiles.length > 0) {
+    const randomNum: number = getRandomNumber(0, tiles.length - 1);
+    const { x, y } = tiles[randomNum];
+    // console.log(`#${indexPlayer} player random attack:`, { x, y });
+    attack(
+      JSON.stringify({
+        gameId,
+        x,
+        y,
+        indexPlayer: ws.index,
+      }),
+      ws,
+    );
+  }
+};
+
+export default randomAttack;
