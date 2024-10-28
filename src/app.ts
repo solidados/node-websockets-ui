8e2717227641b8e19e855ae7bf5385f1138ac6ef
solidados <geekbrains.pk@internet.ru>
2024-10-28 12:54:58 +0400

    refactor: update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import { config } from 'dotenv';
 import { httpServer } from './http_server';
-import './websocket_server';
+import { closeWebSocketServer } from './websocket_server';
 
 config();
 
 const HTTP_PORT: number = Number(process.env.HTTP_PORT) || 8181;
-const WS_PORT: number = Number(process.env.WS_PORT) || 3000;
 
-console.log(`Start HTTP server static on PORT: ${HTTP_PORT}`);
-httpServer.listen(HTTP_PORT);
-console.log(`Start WebSocket server on PORT: ${WS_PORT}`);
+httpServer.listen(HTTP_PORT, () =>
+  console.log(
+    `Start HTTP server static on PORT: \t\x1b[42m ${HTTP_PORT} \x1b[0m`,
+  ),
+);
+
+process.on('SIGINT', () => {
+  closeWebSocketServer();
+  httpServer.close(() => {
+    console.log(`\x1b[33mHTTP server closed!\x1b[0m`);
+    process.exit(0);
+  });
+});
